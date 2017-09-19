bb54b601fa2f1233627779690d343f66265ba303
US\NOkram <niristotle.okram@cbre.com>
2017-09-19 14:47:06 -0500

    Added the post route

--- app.js
+++ app.js
@@ -17,11 +17,20 @@ app.get("/", function(req, res){
   res.render("home", {releases:releases});
 });
 
+app.get("/new", function(req, res){
+    res.render("new");
+});
+
 //Create a POST route that accepts new values to be updated, and redirect to the home route
 app.post("/", function(req, res){
-  res.send("you have hit the post page!");
   //get the data
+  var org = req.body.org;
+  var package = req.body.package;
+  var date = req.body.date;
+  var newRelease = {org: org, package: package, date: date}
+  releases.push(newRelease);
   //redirect to the home page
+  res.redirect("/");
 });
 
 app.listen('9090', function(){
